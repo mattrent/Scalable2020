23c2e57005e304d5c103e708dbbbfc47755ea636
Matteo Trentin <matteo.trentin@studio.unibo.it>
2021-01-04 22:15:32 +0100

    Export to .gexf (not useful)

--- src/main/scala/utils/GraphBuilder.scala
+++ src/main/scala/utils/GraphBuilder.scala
@@ -32,4 +32,26 @@ object GraphBuilder {
 			val edgesDataframe = spark.read.option("header", "true").csv("data/musae_git_edges.csv")
 		*/
 	}
+
+	def export[VD, ED](graph: Graph[VD, ED], fileName: String) = {
+		def toGexf[VD,ED](g:Graph[VD,ED]) =
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			  "<gexf xmlns=\"http://www.gexf.net/1.2draft\" version=\"1.2\">\n" +
+			  "  <graph mode=\"static\" defaultedgetype=\"undirected\">\n" +
+			  "    <nodes>\n" +
+			  g.vertices.map(v => "      <node id=\"" + v._1 + "\" label=\"" +
+				v._2 + "\" />\n").collect.mkString +
+			  "    </nodes>\n" +
+			  "    <edges>\n" +
+			  g.edges.map(e => "      <edge source=\"" + e.srcId +
+				"\" target=\"" + e.dstId + "\" label=\"" + e.attr +
+				"\" />\n").collect.mkString +
+			  "    </edges>\n" +
+			  "  </graph>\n" +
+			  "</gexf>"
+
+		val pw = new java.io.PrintWriter(fileName)
+		pw.write(toGexf(graph))
+		pw.close
+	}
 }
