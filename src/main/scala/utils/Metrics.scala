8c2871800b2edda6bbcbbecaff7dc2b56ccabc61
Matteo Trentin <matteo.trentin@studio.unibo.it>
2021-01-08 14:38:36 +0100

    Metrics refactoring

--- src/main/scala/utils/Metrics.scala
+++ src/main/scala/utils/Metrics.scala
@@ -7,67 +7,23 @@ import scala.collection.parallel.ParMap
 
 object Metrics {
 
-	def density_old(graphLPA: Graph[VertexId, Int],graph: Graph[String, Int]): ParMap[VertexId, Int] = {
-		val community=graphLPA.vertices.groupBy(_._2).map(group => (group._1, group._2.map(pair => pair._1))).collectAsMap().par
-		//print(community.foreach(println))
-		val denominatore=graphLPA.vertices.groupBy(_._2).map(group => (group._1,group._2.size*(group._2.size-1)))
-		//print(denominatore.lookup(30360).mkString(",").toInt)
-		//denominatore.foreach(println)
-
-		//val neighbors = graph.collectNeighbors(EdgeDirection.Out).collectAsMap()
-
-		val neighbors=graph.collectNeighborIds(EdgeDirection.Out).collectAsMap().par
-		//print(neighbors(18778).toSet)
-
-		val numberEdgesInCommmunity=community.map(
-			c => (c._1, c._2.map(id => (neighbors(id).toSet.intersect(c._2.toSet)).size).sum)
-		)
-
-		//println(numberEdgesInCommmunity.foreach(println))
-
-		val internalDensity= community.map(
-			c => (c._1, if (denominatore.lookup(c._1).mkString(",").toInt ==0) 0
-			else (2 * numberEdgesInCommmunity (c._1))/denominatore.lookup(c._1).mkString(",").toInt)
-		)
-		//println(internalDensity.foreach(println))
-
-		internalDensity
-
-	}
-
 	def density(graphLPA: Graph[VertexId, Int]): RDD[(VertexId, Float)] = {
 		val community = graphLPA.vertices.groupBy(_._2).map(group => (group._1, group._2.map(pair => pair._1)))
-		//val denominatore = graphLPA.vertices.groupBy(_._2).map(group => (group._1, group._2.size * (group._2.size - 1)))
 		val neighbors = graphLPA.collectNeighborIds(EdgeDirection.Out).collectAsMap().par
 
-
-
-		def denominatore(community: (VertexId, Iterable[VertexId])): Float = {
-			community._2.size * (community._2.size - 1).toFloat
-		}
-
-		def numberEdgesInCommmunity(community: (VertexId, Iterable[VertexId]), neighbors: ParMap[VertexId, Array[VertexId]]): Float = {
-			community._2.map(id => (neighbors(id).intersect(community._2.toSeq.par)).size).sum.toFloat
-		}
-
-		//val neighbors = graph.collectNeighbors(EdgeDirection.Out).collectAsMap()
-
-
-		/*val numberEdgesInCommmunity = community.map(
-			c => (c._1, c._2.map(id => (neighbors(id).intersect(c._2.toSeq.par)).size).sum)
-		)*/
-
-		//println(numberEdgesInCommmunity.foreach(println))
-
-		/*val internalDensity = community.map(
-			c => (c._1, if (denominatore.lookup(c._1).mkString(",").toInt ==0) 0
-			else (2 * numberEdgesInCommmunity(c._1)) / denominatore.lookup(c._1).mkString(",").toInt)
-		)*/
-
-		val internalDensity = community.map(c => (c._1,if (denominatore(c) == 0) 0F
-		else (2 * numberEdgesInCommmunity(c, neighbors)) / denominatore(c)))
+		val internalDensity = community.map(c => (c._1,
+		  if (possibleCommunityEdges(c) == 0) 0F
+		  else (2 * internalCommunityEdges(c, neighbors)) / internalCommunityEdges(c))
+		)
 
 		internalDensity
+	}
 
+	private def possibleCommunityEdges(community: (VertexId, Iterable[VertexId])): Float = {
+		community._2.size * (community._2.size - 1).toFloat
+	}
+
+	private def internalCommunityEdges(community: (VertexId, Iterable[VertexId]), neighbors: ParMap[VertexId, Array[VertexId]]): Float = {
+		community._2.map(id => (neighbors(id).intersect(community._2.toSeq.par)).size).sum.toFloat
 	}
 }
