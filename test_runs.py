382939532ecd6b9f55d6620bca9ce2ad67987f61
Matteo Trentin <matteo.trentin@studio.unibo.it>
2021-01-19 15:52:55 +0100

    Corrections in test run script

--- test_runs.py
+++ test_runs.py
@@ -1,8 +1,8 @@
 import subprocess
 
 filename = "target/scala-2.12/Scalable2020.jar"
-steps = range(1,51)
-algorithms = ["LPA","LPA_spark","DLPA","SLPA"]
+steps = range(5, 55, 5)
+algorithms = ["LPA","LPA_spark","DLPA", "LPA_pregel"]
 metrics = "true"
 communities = "true"
 nodeFile = "data/musae_git_target.csv"
@@ -26,3 +26,19 @@ for alg in algorithms:
                  "--communities", communities,
                  "--steps", str(step_num),
                  "--results", results])
+
+for simplified in simplify:
+    for step_num in steps:
+        print("SLPA", simplified, step_num) 
+        subprocess.run(
+            ["spark-submit", 
+                "--master", "local[*]",
+                filename,
+                "--vertices", nodeFile,
+                "--edges", edgeFile,
+                "--simplify", simplified,
+                "--metrics", "false",
+                "--algorithm", "SLPA",
+                "--communities", communities,
+                "--steps", str(step_num),
+                "--results", results])
